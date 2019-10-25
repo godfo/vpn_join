045182eaf8dd4107ef205a184fc8caea46372344
actantion <672817221@qq.com>
2019-10-25 22:41:06 +0800

    fix sh bug

--- start_node.sh
+++ start_node.sh
@@ -28,7 +28,7 @@ check_status(){
 
 replace_ip() {
     ip=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
-    sed -i 's/replace_local_ip/'$ip'/g' ./node/conf/lego/conf
+    sed -i 's/replace_local_ip/'$ip'/g' ./node/conf/lego.conf
 }
 
 start(){
@@ -36,7 +36,7 @@ start(){
     init_sys
     replace_ip
     check_status
-    cd ./node && nohup ./vpn_svr -f 0 -g 0 &
+    cd ./node && nohup ./net -f 0 -g 0 &
 }
 
 start
